bce9179c67a5604c4de13c823763bcf8e4fcf15b
ahonafredowanhasib <4hon4f.redowan.hasib.03@gmail.com>
2025-09-09 01:23:39 +0600

    jackfruit and banana tree add to cart button set with your cart section by js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+  
+//   card 1
   const cart = []
   const cartBox = document.getElementById("yourCart")
   const addBtn = document.getElementById("card1")
@@ -5,6 +7,73 @@
   addBtn.addEventListener("click", function() {
     const name = document.getElementById("mango-tree").innerText
     const price = document.getElementById("mango-tree-price").innerText
+
+    // cart array add
+    cart.push({ name, price })
+
+    // UI update
+    const item = document.createElement("p")
+    item.innerText = name + " - " + price + " ";
+
+    // Cancel button
+    const cancelBtn = document.createElement("button")
+    cancelBtn.innerText = "❌"
+    cancelBtn.style.cursor = "pointer"
+    cancelBtn.style.marginLeft = "10px"
+
+    // cancel button click event
+    cancelBtn.addEventListener("click", function() {
+      cartBox.removeChild(item);
+      const index = cart.findIndex(i => i.name === name && i.price === price);
+      if(index > -1) cart.splice(index, 1)
+    })
+    item.appendChild(cancelBtn)
+    cartBox.appendChild(item)
+  })
+
+
+//   card 2
+
+const cart2 = []
+  const cartBox2 = document.getElementById("yourCart")
+  const addBtn2 = document.getElementById("card2")
+
+  addBtn2.addEventListener("click", function() {
+    const name = document.getElementById("jack-tree").innerText
+    const price = document.getElementById("jack-tree-price").innerText
+
+    // cart array add
+    cart.push({ name, price })
+
+    // UI update
+    const item = document.createElement("p")
+    item.innerText = name + " - " + price + " ";
+
+    // Cancel button
+    const cancelBtn = document.createElement("button")
+    cancelBtn.innerText = "❌"
+    cancelBtn.style.cursor = "pointer"
+    cancelBtn.style.marginLeft = "10px"
+
+    // cancel button click event
+    cancelBtn.addEventListener("click", function() {
+      cartBox.removeChild(item);
+      const index = cart.findIndex(i => i.name === name && i.price === price);
+      if(index > -1) cart.splice(index, 1)
+    })
+    item.appendChild(cancelBtn)
+    cartBox.appendChild(item)
+  })
+
+//   card 3
+
+const cart3 = []
+  const cartBox3 = document.getElementById("yourCart")
+  const addBtn3 = document.getElementById("card3")
+
+  addBtn3.addEventListener("click", function() {
+    const name = document.getElementById("banana-tree").innerText
+    const price = document.getElementById("banana-tree-price").innerText
 
     // cart array add
     cart.push({ name, price })
