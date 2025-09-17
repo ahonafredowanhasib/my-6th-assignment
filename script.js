dac1b550c2b35a0e45c462f93178e3659460c6fe
ahonafredowanhasib <4hon4f.redowan.hasib.03@gmail.com>
2025-09-18 03:35:01 +0600

    100% pc responsive correction

--- script.js
+++ script.js
@@ -34,17 +34,17 @@ const displayPost=(posts)=>{
         // 2. creat element
         const postCard = document.createElement("div")
         postCard.innerHTML = `
-        <div class="post-card place-items-center mt-2 bg-white h-140 w-85 rounded-2xl p-3">
+        <div class="post-card place-items-center mt-2 bg-white h-140 w-75 rounded-2xl p-3">
             <img class=" rounded-3xl h-65 w-80" src="${post.image}" alt="${post.name}">
             <h2 class="ml-0 text-[#1F2937] text-xl mt-1 inter-sem">${post.name}</h2>
-            <div class="h-28 mw-100% mb-5 mt-1"> 
-            <p class="text-[#1F2937] inter-nor">${post.description}</p>
+            <div class="h-25 mw-100% mb-3 mt-1"> 
+            <p class="text-[#1F2937] inter-nor text-sm">${post.description}</p>
             </div>
-            <div class="flex gap-[150px] items-center">
-            <h5 class="bg-[#DCFCE7] text-[#15803D] rounded-3xl p-2 w-[120px] text-center">${post.category}</h5>
+            <div class="flex gap-[110px] items-center">
+            <h5 class="bg-[#DCFCE7] text-[#15803D] rounded-3xl p-2 w-[125px] text-center">${post.category}</h5>
             <h4 class="inter-sem">৳${post.price}</h4>
             </div>
-            <button id="card1" type="button" class="bg-[#15803D] text-white px-6 py-2 rounded-4xl font-semibold hover:bg-[#116530] transition mx-auto block mt-2 w-[90%] max-w-[240px]">Add to Cart</button>
+            <button id="card1" type="button" class="bg-[#15803D] text-white px-6 py-2 rounded-4xl font-semibold hover:bg-[#116530] transition mx-auto block mt-4 w-[90%] max-w-[240px]">Add to Cart</button>
         </div>`
         // add ali into container
         postContainer.append(postCard)
