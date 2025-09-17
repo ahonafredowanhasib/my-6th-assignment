fa5fa34b71c73351f8bc2704dc20827caea55601
ahonafredowanhasib <4hon4f.redowan.hasib.03@gmail.com>
2025-09-18 03:24:29 +0600

    api add and all trees click add

--- script.js
+++ script.js
@@ -1,91 +1,53 @@
-const cart = []
-const cartBox = document.getElementById("yourCart")
-const sum = document.getElementById("total")
-const allclear = document.getElementById("clearCart")
 
-// cart
-const heading = document.createElement("h2")
-heading.className = "text-2xl font-semibold pt-[10px] pb-[10px]"
-heading.innerText = "Your Cart (0 items)"
-cartBox.insertBefore(heading, sum)
-
-// total + count
-function updateTotal() {
-  let total = 0
-  cart.forEach(item => {
-    total += parseInt(item.price.replace("৳", ""))
-  })
-  sum.innerText = "Total: ৳" + total
-  heading.innerText = `Your Cart (${cart.length} items)`
-}
-
-// Add to cart
-function addToCart(nameId, priceId) {
-  const name = document.getElementById(nameId).innerText
-  const price = document.getElementById(priceId).innerText
-
-  cart.push({ name, price })
-
-  const item = document.createElement("div")
-  item.className = "flex justify-between items-center mt-2 bg-[#F9FAFB] p-2 rounded-md"
-
-  const itemText = document.createElement("span")
-  itemText.innerText = name + " - " + price
-
-  const cancelBtn = document.createElement("button")
-  cancelBtn.innerText = "❌"
-  cancelBtn.style.cursor = "pointer"
-  cancelBtn.addEventListener("click", function () {
-    cartBox.removeChild(item)
-    const index = cart.findIndex(i => i.name === name && i.price === price)
-    if (index > -1) cart.splice(index, 1)
-    updateTotal()
-  })
-
-  item.appendChild(itemText)
-  item.appendChild(cancelBtn)
-  cartBox.insertBefore(item, sum)
-  updateTotal()
-}
-
-// clear
-allclear.addEventListener("click", function () {
-  cart.length = 0
-  const items = cartBox.querySelectorAll(".flex.justify-between")
-  items.forEach(item => item.remove())
-  updateTotal()
-})
-
-
-document.getElementById("card1").addEventListener("click", () => addToCart("mango-tree", "mango-tree-price"))
-document.getElementById("card2").addEventListener("click", () => addToCart("jack-tree", "jack-tree-price"))
-document.getElementById("card3").addEventListener("click", () => addToCart("banana-tree", "banana-tree-price"))
-document.getElementById("card4").addEventListener("click", () => addToCart("Bamboo-tree", "Bamboo-tree-price"))
-document.getElementById("card5").addEventListener("click", () => addToCart("Teak-Tree", "Teak-Tree-price"))
-document.getElementById("card6").addEventListener("click", () => addToCart("Kanthalichapa", "Kanthalichapa-price"))
-document.getElementById("card7").addEventListener("click", () => addToCart("Baby-Coconut-Palm-tree", "Baby-Coconut-Palm-tree-price"))
-document.getElementById("card8").addEventListener("click", () => addToCart("rose", "rose-price"))
-document.getElementById("card9").addEventListener("click", () => addToCart("Marigold", "Marigold-price"))
-document.getElementById("card10").addEventListener("click", () => addToCart("Madhabilata", "Madhabilata-price"))
-document.getElementById("card11").addEventListener("click", () => addToCart("Waterlettuce", "Waterlettuce-price"))
-document.getElementById("card12").addEventListener("click", () => addToCart("Tuberose", "Tuberose-price"))
-
-
-// cards
-const cards = document.querySelectorAll(".grid > section")
-const categoryBtns = document.querySelectorAll(".space-y-3 h5")
-categoryBtns.forEach(btn => {
-  btn.addEventListener("click", () => {
-    const category = btn.innerText.trim()
-
-    cards.forEach(card => {
-      const cardCategory = card.getAttribute("data-category")
-
-      if (category === "All Trees" || cardCategory === category) {
-        card.style.display = "block" 
-      } else {
-        card.style.display = "none" 
-      }
+const loadPost = () => {
+    const url1 = 'https://openapi.programming-hero.com/api/plants'
+    fetch(url1)
+    .then((res) => res.json())
+    .then((data)=>{
+        console.log(data)
+        displayPost(data.plants)
     })
-  })
-})
+}
+
+
+
+// {
+//     "id": 1,
+//     "image": "https://i.ibb.co.com/cSQdg7tf/mango-min.jpg",
+//     "name": "Mango Tree",
+//     "description": "A fast-growing tropical tree that produces delicious, juicy mangoes during summer. Its dense green canopy offers shade, while its sweet fruits are rich in vitamins and minerals.",
+//     "category": "Fruit Tree",
+//     "price": 500
+// }
+
+const displayPost=(posts)=>{
+
+    // 1. get the container
+    const postContainer = document.getElementById("post-container")
+    // console.log(postContainer)
+    postContainer.innerHTML = ''
+
+
+    posts.forEach((post) => {
+        // console.log(post.name)
+
+        // 2. creat element
+        const postCard = document.createElement("div")
+        postCard.innerHTML = `
+        <div class="post-card place-items-center mt-2 bg-white h-140 w-85 rounded-2xl p-3">
+            <img class=" rounded-3xl h-65 w-80" src="${post.image}" alt="${post.name}">
+            <h2 class="ml-0 text-[#1F2937] text-xl mt-1 inter-sem">${post.name}</h2>
+            <div class="h-28 mw-100% mb-5 mt-1"> 
+            <p class="text-[#1F2937] inter-nor">${post.description}</p>
+            </div>
+            <div class="flex gap-[150px] items-center">
+            <h5 class="bg-[#DCFCE7] text-[#15803D] rounded-3xl p-2 w-[120px] text-center">${post.category}</h5>
+            <h4 class="inter-sem">৳${post.price}</h4>
+            </div>
+            <button id="card1" type="button" class="bg-[#15803D] text-white px-6 py-2 rounded-4xl font-semibold hover:bg-[#116530] transition mx-auto block mt-2 w-[90%] max-w-[240px]">Add to Cart</button>
+        </div>`
+        // add ali into container
+        postContainer.append(postCard)
+    });
+    // console.log(posts)
+}
